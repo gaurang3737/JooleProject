7574707309dd94f93dccbd3db2a18f119a4b2245
gaurang3737 <gaurangpatel3737@gmail.com>
2021-01-06 16:50:12 -0500

    Integrated Top Search Bar and Compare Products Page

--- JooleOnlineShop/Controllers/ProductController.cs
+++ JooleOnlineShop/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Joole.DAL;
+using Joole.Service.Models;
 
 namespace JooleOnlineShop.Controllers
 {
@@ -24,15 +25,15 @@ namespace JooleOnlineShop.Controllers
             List<tblProduct> prodlist = new List<tblProduct>();
             List<String> man_list = new List<string>();
             List<List<String>> props_value_list = new List<List<String>>();
-
             Services serv = new Services();
+            List<Category> categories = serv.GetCategoryList();//for top-search bar
             ViewBag.cat_name = serv.GetCategoryName(cat_id);
             ViewBag.sub_cat_name = serv.GetSubCategoryName(sub_id);
             (prodlist, man_list, props_value_list) = serv.GetProductBySubCategoryID(sub_id);
             ViewBag.prod_data = prodlist;
             ViewBag.man_data = man_list;
             ViewBag.props_data = props_value_list;
-            return View("Product");
+            return View("Product", categories);
         }
 
         //GET Product Details
@@ -47,12 +48,51 @@ namespace JooleOnlineShop.Controllers
             ViewBag.Man = productdetails[0];
             ViewBag.IsType = productdetails[1];
             ViewBag.IsTechSpec = productdetails[2];
-            return View("ProductList");
+            List<Category> categories = service.GetCategoryList();//for top-search bar
+            return View("ProductList", categories);
         }
 
+        public ActionResult CompareProducts(int a, int b, int c = -1)
+        {
+            Services service = new Services();
+            if (c == -1)
+            {
+                List<List<List<string>>> product1details = service.GetCompareProducts(a);
+                List<List<List<string>>> product2details = service.GetCompareProducts(b);
+                product1details[0].AddRange(product2details[0]);
+                product1details[1].AddRange(product2details[1]);
+                product1details[2].AddRange(product2details[2]);
+
+                ViewBag.CompareMan = product1details[0];
+                ViewBag.CompareIsType = product1details[1];
+                ViewBag.CompareIsTechSpec = product1details[2];
+                ViewBag.Compare2 = 1;
+            }
+            else
+            {
+                List<List<List<string>>> product1details = service.GetCompareProducts(a);
+                List<List<List<string>>> product2details = service.GetCompareProducts(b);
+                List<List<List<string>>> product3details = service.GetCompareProducts(c);
+                product1details[0].AddRange(product2details[0]);
+                product1details[0].AddRange(product3details[0]);
+
+                product1details[1].AddRange(product2details[1]);
+                product1details[1].AddRange(product3details[1]);
+
+                product1details[2].AddRange(product2details[2]);
+                product1details[2].AddRange(product3details[2]);
+
+                ViewBag.CompareMan = product1details[0];
+                ViewBag.CompareIsType = product1details[1];
+                ViewBag.CompareIsTechSpec = product1details[2];
+                ViewBag.Compare2 = 0;
+            }
+            List<Category> categories = service.GetCategoryList();//for top-search bar
+            return View("CompareProducts", categories);
+        }
         public ActionResult Logout()
         {
-            //Clearing Sessions and Cookies
+            //Clearing Sessions
             Session.RemoveAll();
             return RedirectToAction("Login", "Login");
         }
